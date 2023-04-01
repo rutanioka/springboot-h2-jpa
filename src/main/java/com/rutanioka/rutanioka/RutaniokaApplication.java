1b438d685c54e7d63178203cb2a4d005c63ed0ee
Rubens Tanioka <87711124+rutanioka@users.noreply.github.com>
2023-04-01 10:59:29 -0300

    pastas e edição iniciadda

---
+++ src/main/java/com/rutanioka/rutanioka/RutaniokaApplication.java
@@ -0,0 +1,13 @@
+package com.rutanioka.rutanioka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RutaniokaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RutaniokaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rutanioka/rutanioka/RutaniokaApplicationTest.java
@@ -0,0 +1,13 @@
+package com.rutanioka.rutanioka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RutaniokaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
